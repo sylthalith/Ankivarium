5199d3e5da40cd69f3480579c40050fb805c090b
sylthalith <sylthalith@gmail.com>
2025-08-07 14:04:57 +0500

    Динамический вывод колод на dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,55 +3,66 @@
         @vite(['resources/css/dashboard.css'])
     @endpush
 
+    @push('scripts')
+        @vite(['resources/js/dashboard.js'])
+    @endpush
+
     <header>
         <h1>Мои колоды</h1>
         <div class="search-menu">
             <input type="search" placeholder="Поиск колод...">
-            <button class="btn">
-                <img class="search" src="{{ Vite::asset('resources/images/search.png') }}">
+            <button class="red-btn">
+                <img class="search" src="{{ Vite::asset('resources/images/SearchOutlined.svg') }}">
             </button>
         </div>
     </header>
 
-    <button class="btn new-deck">
-        <img class="plus" src="{{ Vite::asset('resources/images/plus.png') }}">
+    <button class="red-btn new-deck">
+        <img class="plus" src="{{ Vite::asset('resources/images/Plus.svg') }}">
         Новая колода
     </button>
     <div class="decks">
-    @for ($i = 0; $i < 4; $i++)
-        <div class="deck">
+    @foreach ($decks as $deck)
+        @php
+            $cardsDue =  $stats[$deck->id]->cards_due;
+            $cardsCompleted = $stats[$deck->id]->cards_completed;
+        @endphp
+        <div class="deck" data-deck-id="{{ $deck->id }}">
             <div class="deck-title">
-                <h3>Длинное название для колоды</h3>
+                <h3>{{ $deck->name }}</h3>
             </div>
             <div class="to-study">
-                100 карточек
+                {{ $cardsDue }} карточек
             </div>
             <div class="progress-bar">
-                <progress max="100" value="30"></progress>
+                <progress max="{{ $cardsDue }}" value="{{ $cardsCompleted }}"></progress>
             </div>
             <div class="progress">
-                Изучено: 30/100 (30%)
+                Изучено: {{ $cardsCompleted }}/{{ $cardsDue }} ({{ $cardsDue == 0 ? 0 : round($cardsCompleted / $cardsDue)}}%)
             </div>
             <div class="actions">
-                <button class="action-btn">
-                    <img src="{{ Vite::asset('resources/images/start.png') }}">
+                <button class="btn action-btn study-btn">
+                    <img src="{{ Vite::asset('resources/images/Play.svg') }}">
                     Учить
                 </button>
-                <button class="action-btn">
-                    <img src="{{ Vite::asset('resources/images/plus.png') }}">
-                    Добавить
-                </button>
-                <button class="action-btn">
-                    <img src="{{ Vite::asset('resources/images/edit.png') }}">
+                <form method="GET" action="{{ route('cards.create', ['deck' => $deck]) }}">
+                    <input type="hidden" name="deck_id" value="{{ $deck->id }}">
+                    <button type="submit" class="btn action-btn add-btn">
+                        <img src="{{ Vite::asset('resources/images/Plus.svg') }}">
+                        Добавить
+                    </button>
+                </form>
+                <button class="btn action-btn edit-btn">
+                    <img src="{{ Vite::asset('resources/images/EditOutlined.svg') }}">
                     Изменить
                 </button>
-                <button class="action-btn">
-                    <img src="{{ Vite::asset('resources/images/delete.png') }}">
+                <button class="btn action-btn delete-btn">
+                    <img src="{{ Vite::asset('resources/images/DeleteOutlined.svg') }}">
                     Удалить
                 </button>
             </div>
         </div>
-    @endfor
+    @endforeach
     </div>
 
     <form method="POST" action="{{ route('logout') }}">
