d37b7aabe99b051b6841f6b94072bef66b4b3730
sylthalith <sylthalith@gmail.com>
2025-08-23 14:26:57 +0500

    Svg иконки в обучении

--- resources/views/study/create.blade.php
+++ resources/views/study/create.blade.php
@@ -30,22 +30,22 @@
                     <div id="back-text" class="card-text"></div>
                     <div id="back-actions" class="actions">
                         <button id="again" class="difficulty-btn">
-                            <img src="{{ Vite::asset('resources/images/FaceSad.svg') }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke-linecap="round" stroke-width="2"><circle cx="12" cy="12" r="10"/><path d="M8 9.05v-.1m8 .1v-.1"/><path stroke-linejoin="round" d="M16 16c-.5-1.5-1.79-3-4-3s-3.5 1.5-4 3"/></g></svg>
                             <div class="difficulty-btn-text">Снова</div>
                             <div class="time"></div>
                         </button>
                         <button id="hard" class="difficulty-btn">
-                            <img src="{{ Vite::asset('resources/images/FaceNeutral.svg') }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke-linecap="round" stroke-width="2"><circle cx="12" cy="12" r="10"/><path d="M8 9.05v-.1m8 .1v-.1M8 14h8"/></g></svg>
                             <div class="difficulty-btn-text">Трудно</div>
                             <div class="time"></div>
                         </button>
                         <button id="good" class="difficulty-btn">
-                            <img src="{{ Vite::asset('resources/images/FaceHappy.svg') }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke-linecap="round" stroke-width="2"><circle cx="12" cy="12" r="10"/><path d="M8 9.05v-.1m8 .1v-.1"/><path stroke-linejoin="round" d="M16 14c-.5 1.5-1.79 3-4 3s-3.5-1.5-4-3"/></g></svg>
                             <div class="difficulty-btn-text">Хорошо</div>
                             <div class="time"></div>
                         </button>
                         <button id="easy" class="difficulty-btn">
-                            <img src="{{ Vite::asset('resources/images/FaceVeryHappy.svg') }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke-linecap="round" stroke-width="2"><circle cx="12" cy="12" r="10"/><path d="M8 9.05v-.1m8 .1v-.1"/><path stroke-linejoin="round" d="M12 18a4 4 0 0 0 4-4H8a4 4 0 0 0 4 4"/></g></svg>
                             <div class="difficulty-btn-text">Легко</div>
                             <div class="time"></div>
                         </button>
